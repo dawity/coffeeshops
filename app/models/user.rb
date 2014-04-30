e7cad170a12266c9b2030695e31d465669909147
Dawity <dawityehaile@gmail.com>
2014-04-29 23:00:58 -0700

    Rebased and pushed to gitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
