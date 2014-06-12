84306fb7f7bb3144c3ea6901d8607e2bfa8cceaf
Dawity <dawityehaile@gmail.com>
2014-06-12 15:35:22 -0700

    email to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Ganjamap::Application.routes.draw do
   resources :ganjalocs
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  
+  post 'incoming_emails' => 'incoming_emails#create'
 
   root 'home#index', as: 'home'
   match '/signup',  to: 'users#new',            via: 'get'
