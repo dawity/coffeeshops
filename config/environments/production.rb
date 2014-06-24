d9e262ab2dbed9bbd66efff80b7d49f5bb6dbd90
Dawity <dawityehaile@gmail.com>
2014-06-23 17:45:28 -0700

    Fixed responsive search box

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Ganjamap::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  #config.assets.css_compressor = :sass
-   config.assets.css_compressor = :yui
+  config.assets.css_compressor = :sass
+  #config.assets.css_compressor = :yui
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -90,4 +90,5 @@ Ganjamap::Application.configure do
  #    :sender_address => %{"notifier" <dawityhaile@gmail.com>},
  #    :exception_recipients => %w{dawityhaile@gmail.com}
  #  }
+ config.active_record.dump_schema_after_migration = false
 end
