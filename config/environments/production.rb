42e2d770c5bea96e1d5d11731be2b764bc01e52e
Dawity <dawityehaile@gmail.com>
2014-06-29 00:32:05 -0700

    added needed gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Ganjamap::Application.configure do
   config.assets.css_compressor = :sass
   #config.assets.css_compressor = :yui
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
